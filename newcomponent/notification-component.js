a2d1adbc6d90dd8f4efbcc7cf8211c28c5501f90
Tungalagtamir1864 <112697493+Tungalagtamir1864@users.noreply.github.com>
2023-12-23 22:43:05 +0800

    vdksl

--- newcomponent/notification-component.js
+++ newcomponent/notification-component.js
@@ -1,32 +1,36 @@
-
-
-class NotificationComponent extends LitElement {
-  static styles = css`
-
-  `;
-
-  static properties = {
-    wcShow: { type: Boolean, attribute: 'wc-show' },
-    message: { type: String }
-  };
-
+class NotificationComponent extends HTMLElement {
   constructor() {
     super();
-    this.wcShow = false;
-    this.message = '';
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
+      <style>
+        :host {
+          display: none;
+          position: fixed;
+          top: 20px;
+          right: 20px;
+          padding: 10px;
+          background-color: #28a745;
+          color: #fff;
+          border-radius: 4px;
+          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        }
+      </style>
+      <div id="notificationContent">Хүсэлт илгээгдлээ</div>
+    `;
   }
 
   showNotification(message) {
-    this.message = message;
-    this.wcShow = true;
-
-    setTimeout(() => {
-      this.wcShow = false;
-    }, 3000);
-  }
-
-  render() {
-    return html`${this.message}`;
+    const notificationContent = this.shadowRoot.getElementById('notificationContent');
+    if (notificationContent) {
+      notificationContent.textContent = message || 'Хүсэлт илгээгдлээ';
+      this.style.display = 'block';
+      setTimeout(() => {
+        this.style.display = 'none';
+      }, 3000);
+    } else {
+      console.error('Энэ хуудасанд хүсэлт илгээгдлээг нь үзүүлэхийн тулд "notificationContent" id-тэй элемент хэрэгтэй байна.');
+    }
   }
 }
 
--- newcomponent/user-card.js
+++ newcomponent/user-card.js
@@ -1,11 +1,71 @@
-customElements.define('user-card', class extends HTMLElement {
-  connectedCallback() {
-    this.attachShadow({mode: 'open'});
-    this.shadowRoot.innerHTML = `<input type="submit" value="Илгээх">`;
-    this.shadowRoot.firstElementChild.onclick =
-      e => alert("АМЖИЛТТАЙ ИЛГЭЭГДЛЭЭ");
+// user-card.js
+import './notification-component.js';
+
+class UserCard extends HTMLElement {
+  static get observedAttributes() {
+    return ['dark-mode'];
   }
-});
 
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
+      <style>
+        :host {
+          display: block;
+          --button-bg: #007BFF;
+          --button-text: #fff;
+        }
 
-window.customElements.define('user-card', user-card);
+        input[type="submit"] {
+          padding: 10px;
+          background-color: var(--button-bg);
+          color: var(--button-text);
+          border: none;
+          border-radius: 4px;
+          cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+          background-color: orange;
+        }
+      </style>
+      <input type="submit" value="Илгээх">
+    `;
+    this.shadowRoot
+      .querySelector('input[type="submit"]')
+      .addEventListener('click', (e) => {
+        console.log('New Super Button object has been instantiated.');
+
+        // NotificationComponent-ийг олох
+        const notificationComponent = this.closest('user-card').querySelector('notification-component');
+        if (notificationComponent) {
+          notificationComponent.showNotification('Хүсэлт илгээгдлээ');
+        } else {
+          console.error('NotificationComponent-ийг олох боломжгүй байна.');
+        }
+      });
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'dark-mode') {
+      if (newValue === 'true') {
+        this.setDarkMode();
+      } else {
+        this.setLightMode();
+      }
+    }
+  }
+
+  setDarkMode() {
+    this.shadowRoot.style.setProperty('--button-bg', '#333');
+    this.shadowRoot.style.setProperty('--button-text', '#fff');
+  }
+
+  setLightMode() {
+    this.shadowRoot.style.setProperty('--button-bg', '#007BFF');
+    this.shadowRoot.style.setProperty('--button-text', '#fff');
+  }
+}
+
+customElements.define('user-card', UserCard);
